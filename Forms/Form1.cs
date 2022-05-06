59d346182e85b84a8a34d7dd3145a96bda7d8531
Mitch Bowser <mitchbowser1990@gmail.com>
2022-05-06 03:00:11 +0000

    figured out the language thingy

--- Forms/Form1.cs
+++ Forms/Form1.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
+using System.Threading;
 using SoftwareTwoProject.Forms;
 using SoftwareTwoProject.Class;
 using MySql.Data.MySqlClient;
@@ -16,11 +18,29 @@ namespace SoftwareTwoProject
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
+            //note below confirms if not english then french as culture
+            //note need to add in french message boxes... should just be a simple if statement regarding what the current culture == as a string
+            
+           
+            if(CultureInfo.CurrentCulture.ToString()=="en-US")
+            {
+
+            }
+            else
+            {
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("fr");
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("fr");
+
+            }
+
             InitializeComponent();
             string userid = NameBox.Text;
             MySqlConnection user = new MySqlConnection("server=127.0.0.1;user id=sqlUser;database=client_schedule;port=3306;password=Passw0rd!");
+            
+            MessageBox.Show(CultureInfo.CurrentCulture.ToString());
             
             user.Open();
             MySqlCommand namecommand = new MySqlCommand(logNameQuery,user);
@@ -69,6 +89,7 @@ namespace SoftwareTwoProject
                 else
                 {
                     y = 0;
+                    MessageBox.Show("Login not detected");
                 }
 
 
